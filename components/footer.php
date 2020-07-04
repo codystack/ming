1041c23cc2e3d6963e302c5ef45aa3a89ce9ecb3
codystack <ebuzzadvert@gmail.com>
2020-07-04 01:10:42 +0100

    footer added

---
+++ components/footer.php
@@ -0,0 +1,83 @@
+
+    <section id="contact" class="s-contact">
+        <div class="row section-header" data-aos="fade-up">
+            <div class="col-full">
+                <h3 class="subhead subhead--light">Contact Us</h3>
+                <h1 class="display-1 display-1--light">Let’s talk,<br>Get in touch and we promise to provide answers to all your enquiries.</h1>
+            </div>
+        </div> 
+        <div class="row">
+            <div class="col-full contact-main" data-aos="fade-up">
+                <p>
+                <a href="mailto:info@missinnovationng.org" class="contact-email">info@missinnovationng.org</a>
+                <span class="contact-number">07062976582 || 09031236802</span>
+                </p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-full cl-copyright">
+                <span>
+                &copy;<script>document.write(new Date().getFullYear());</script> All Rights Reserved | Built by <a href="https://webify.com.ng" target="_blank">Webify&trade;</a>
+                </span> 
+            </div>
+        </div>
+
+        <div class="cl-go-top">
+            <a class="smoothscroll" title="Back to Top" href="#top"><i class="icon-arrow-up" aria-hidden="true"></i></a>
+        </div>
+
+    </section> <!-- end s-contact -->
+
+
+    <!-- photoswipe background
+    ================================================== -->
+    <div aria-hidden="true" class="pswp" role="dialog" tabindex="-1">
+
+        <div class="pswp__bg"></div>
+        <div class="pswp__scroll-wrap">
+
+            <div class="pswp__container">
+                <div class="pswp__item"></div>
+                <div class="pswp__item"></div>
+                <div class="pswp__item"></div>
+            </div>
+
+            <div class="pswp__ui pswp__ui--hidden">
+                <div class="pswp__top-bar">
+                    <div class="pswp__counter"></div><button class="pswp__button pswp__button--close" title="Close (Esc)"></button> <button class="pswp__button pswp__button--share" title=
+                    "Share"></button> <button class="pswp__button pswp__button--fs" title="Toggle fullscreen"></button> <button class="pswp__button pswp__button--zoom" title=
+                    "Zoom in/out"></button>
+                    <div class="pswp__preloader">
+                        <div class="pswp__preloader__icn">
+                            <div class="pswp__preloader__cut">
+                                <div class="pswp__preloader__donut"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
+                    <div class="pswp__share-tooltip"></div>
+                </div><button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)"></button> <button class="pswp__button pswp__button--arrow--right" title=
+                "Next (arrow right)"></button>
+                <div class="pswp__caption">
+                    <div class="pswp__caption__center"></div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+    <div id="preloader">
+        <div id="loader">            
+        </div>
+    </div>
+
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+
+</body>
+
+</html>
